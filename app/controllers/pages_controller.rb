3c4b3e4715e658160c726727c24819cbba03306a
fifart <fifart@gmail.com>
2018-06-07 14:32:10 +0300

    request scaffold destroyed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
     def home
+        @avail = Availability.all.order(created_at: :desc)
+        @req = Request.all.order(created_at: :desc)
+        @adverts = (@req + @avail)
+
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :requests
   devise_for :users
   resources :availabilities
   root "pages#home"
