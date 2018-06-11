f69914e3498ea75fce840c1a24a6b2b00d2615ce
fifart <fifart@gmail.com>
2018-06-11 16:18:36 +0300

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+    as :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end 
   resources :availabilities, only: [:show, :update, :destroy, :new, :create, :edit]
   get '/ask', to: "pages#ask"
   get '/declare', to: "pages#declare"
